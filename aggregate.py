7b8c6ff2cab70ae3927198271349c2840adb23e1
amosbrazzoli <amos.brazzoli@gmail.com>
2022-01-02 20:07:06 +0100

    fixed

--- aggregate.py
+++ aggregate.py
@@ -7,8 +7,7 @@ f = []
 for (dirpath, dirnames, filenames) in os.walk("data"):
     f.extend(filenames)
 
-EXCLUDE = ["EDIANAGRAFESTA20181920180901.csv"]
-
+EXCLUDE = ["EDIANAGRAFESTA20181920180901.csv","SCUANAGRAFESTAT20212220210901.csv"]
 
 for e in EXCLUDE:
     f.remove(e)
@@ -27,4 +26,18 @@ for file in tqdm(f):
         df = pd.merge(df, t_df, on=SORTER, how="left")
         print(df.shape, df.memory_usage().sum() / 1024 ** 2, sep="\t")
 
+# Aggiunge anagrafica sul tipo di scuola
+ana_df = pd.read_csv("data/{}".format(EXCLUDE[1]))
+ana_df = ana_df.sort_values(by=["CODICESCUOLA"])
+PURGE = ["ANNOSCOLASTICO",
+            "INDIRIZZOSCUOLA", 
+            "CAPSCUOLA",
+            "DESCRIZIONECOMUNE",
+            "CODICECOMUNESCUOLA",
+            ]
+ana_df = ana_df.drop(PURGE, axis=1)
+df = pd.merge(ana_df, df, on=["CODICESCUOLA"], how="left")
+
+print("Saving will take some time ... please wait")
 df.to_excel("SNAES-1819.xlsx")
+print("Done")
